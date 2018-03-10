5e78a79ee8004399ee56c8dea20a93e6de3de7cb
lellebebbl <nutella.pfannekuchen@gmail.com>
2018-03-10 13:04:03 +0100

    finished color.cpp

--- color.cpp
+++ color.cpp
@@ -1,36 +1,51 @@
 #include "color.h"
 #include <assert.h>
 
+/*
+ * Set Color Black
+ */
 Color::Color()
 {
-  // TODO: add your code
+    this->R = 0;
+    this->G = 0;
+    this->B = 0;
 }
 
 Color::Color( float r, float g, float b)
 {
-	// TODO: add your code
+	this->R = r;
+	this->G = g;
+	this->B = b;
 }
 
 Color Color::operator*(const Color& c) const
 {
-	// TODO: add your code
-    return Color(); // dummy (remove)
+	float new_r = this->R * c.R;
+    float new_g = this->G * c.G;
+    float new_b = this->B * c.B;
+    return Color(new_r, new_g, new_b);
 }
 
 Color Color::operator*(const float Factor) const
 {
-	// TODO: add your code
-	return Color(); // dummy (remove)
+    float new_r = this->R * Factor;
+    float new_g = this->G * Factor;
+    float new_b = this->B * Factor;
+    return Color(new_r, new_g, new_b);
 }
 
 Color Color::operator+(const Color& c) const
 {
-	// TODO: add your code
-	return Color(); // dummy (remove)
+    float new_r = this->R + c.R;
+    float new_g = this->G + c.G;
+    float new_b = this->B + c.B;
+    return Color(new_r, new_g, new_b);
 }
 
 Color& Color::operator+=(const Color& c)
 {
-	// TODO: add your code
+	this->R+= c.R;
+    this->G+= c.G;
+    this->B+= c.B;
 	return *this; // dummy (remove)
 }

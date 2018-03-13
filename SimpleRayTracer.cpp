0b5c87a806558cea747ef31e42e3046b7aed5cdb
lellebebbl <lukas.guenther94@web.de>
2018-03-13 17:43:07 +0100

    updated first fct

--- SimpleRayTracer.cpp
+++ SimpleRayTracer.cpp
@@ -12,11 +12,12 @@ void SimpleRayTracer::traceScene(const Scene &SceneModel, RGBImage &Image) {
 }
 
 Color SimpleRayTracer::trace(const Scene &SceneModel, const Vector &o, const Vector &d, int depth) {
+
     return Color();
 }
 
 Color SimpleRayTracer::localIllumination(const Vector &SurfacePoint, const Vector &Eye, const Vector &Normal,
                                          const PointLight &Light, const Material &Material) {
-    return Color();
+    return Material.getDiffuseCoeff(SurfacePoint);
 }
 
